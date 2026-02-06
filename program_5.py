f0ffee2ec15c36e1fece84e9c020e1124fdd54f3
Slabe1 <26aphi@isd500.com>
2026-02-06 17:02:07 -0600

    Abraham Phillips 2/6/26 Week 3 Programs

--- program_5.py
+++ program_5.py
@@ -4,3 +4,38 @@
 # Also tax is 7%. 
 # Write a program the inputs the type of hot dog wanted and optional toppings.  
 # The program then displays the hot dog cost, tax and total cost. 
+
+cost = 3.5
+
+chili_dog = input("Would you like a chili dog? (y or n) ")
+
+if chili_dog == 'y':
+    cost += 1
+
+cheese = input("Would you like cheese? (y or n) ")
+
+if cheese == 'y':
+    cost += 0.5
+
+peppers = input("Would you like peppers? (y or n) ")
+
+if peppers == 'y':
+    cost += 0.75
+
+grilled_onions = input("Would you like grilled onions? (y or n) ")
+
+if grilled_onions == 'y':
+    cost += 1
+
+print(f"Cost of Hot Dog: {cost}")
+
+tax = cost*0.07
+
+print(f"tax: {tax:.2f}")
+
+total_cost = tax + cost
+
+print(f"total cost: {total_cost:.2f}")
+
+
+
